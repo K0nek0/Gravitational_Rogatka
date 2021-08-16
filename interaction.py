84e680298caf9b10bab1cd6ec273e619312c3e2e
Nikolay <56084697+K0nek0@users.noreply.github.com>
2021-08-16 17:15:02 +0000

    good

--- interaction.py
+++ interaction.py
@@ -1,14 +1,11 @@
-# import numpy as np
-# from scipy.integrate import odeint
-
 G = 6.67 * 10**(-11)
 ae = 149.6 * 10**9
 m_c = 1.9885 * 10**30
 x_c = 0
 y_c = 0
 
-T = 100
-t_end = 1000
+T = 500
+t_end = 100
 k = 200
 
 r = 15
@@ -21,18 +18,6 @@ class Interaction:
         self.vx0 = vx0
         self.y0 = y0
         self.vy0 = vy0
-        # self.s0 = x0, vx0, y0, vy0
-
-    # def func(self, s, t):
-    #     x, vx, y, vy = s
-
-    #     dxdt = vx
-    #     dvxdt = (-G * m_c * (x - x_c) / ((x - x_c)**2 + (y - y_c)**2)**1.5)
-
-    #     dydt = vy
-    #     dvydt = (-G * m_c * (y - y_c) / ((x - x_c)**2 + (y - y_c)**2)**1.5)
-
-    #     return dxdt, dvxdt, dydt, dvydt
 
     def solver(self):
         x_ar=[]
@@ -43,10 +28,6 @@ class Interaction:
         vx_ar.append(self.vx0)
         y_ar.append(self.y0)
         vy_ar.append(self.vy0)
-        print(x_ar)
-        print(vx_ar)
-        print(y_ar)
-        print(vy_ar)
 
         for i in range(t_end):
             self.x0 += T * self.vx0
@@ -61,16 +42,9 @@ class Interaction:
 
 
     def solve_func(self, i):
-       
-        # sol = odeint(self.func, self.s0, (t[i], t[i+1]))
         self.x = self.solver()[0][i]
         self.vx = self.solver()[1][i]
         self.y = self.solver()[2][i]
         self.vy = self.solver()[3][i]
-        # self.x = sol[1, 0]
-        # self.y = sol[1, 2]
-        # self.vx = sol[1, 1]
-        # self.vy = sol[1, 3]
-
-        # self.s0 = self.x, self.vx, self.y, self.vy
+        
         return self.x / ae, self.y / ae
