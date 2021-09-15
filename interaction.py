9420505f3d70cbb93530d1cde9a7d67e391c5aa2
Nikolay <56084697+K0nek0@users.noreply.github.com>
2021-09-15 19:43:59 +0200

    new

--- interaction.py
+++ interaction.py
@@ -1,20 +1,4 @@
-import numpy as np
-from scipy.integrate import odeint
-
-G = 6.67 * 10**(-11)
-ae = 149.6 * 10**9
-m_c = 1.9885 * 10**30
-x_c = 0
-y_c = 0
-
-T = 365
-t_end = 2 * 24 * 3600 * 365
-t = np.linspace(0, t_end, T+1)
-k = 200
-
-r = 15
-w = 400
-h = 300
+import variables as var
 
 class Interaction:
     def __init__(self, x0, vx0, y0, vy0):
@@ -22,28 +6,36 @@ class Interaction:
         self.vx0 = vx0
         self.y0 = y0
         self.vy0 = vy0
-        self.s0 = self.x0, self.vx0, self.y0, self.vy0
 
-    def func(self, s, t):
-        x, vx, y, vy = s
+    def solver(self):
+        x_ar=[] 
+        vx_ar=[]
+        y_ar=[]
+        vy_ar=[]
+        x_ar.append(self.x0)
+        vx_ar.append(self.vx0)
+        y_ar.append(self.y0)
+        vy_ar.append(self.vy0)
 
-        dxdt = vx
-        dvxdt = (-G * m_c * (x - x_c) / ((x - x_c)**2 + (y - y_c)**2)**1.5)
+        for i in range(var.t_end):
+            self.x0 += var.T * self.vx0
+            self.vx0 += var.T * (-var.G * var.m_c * (self.x0 - var.x_c) / ((self.x0 - var.x_c)**2 + (self.y0 - var.y_c)**2)**1.5)
 
-        dydt = vy
-        dvydt = (-G * m_c * (y - y_c) / ((x - x_c)**2 + (y - y_c)**2)**1.5)
+            self.y0 += var.T * self.vy0
+            self.vy0 += var.T * (-var.G * var.m_c * (self.y0 - var.y_c) / ((self.x0 - var.x_c)**2 + (self.y0 - var.y_c)**2)**1.5)
 
-        return dxdt, dvxdt, dydt, dvydt
+            x_ar.append(self.x0)
+            vx_ar.append(self.vx0)
+            y_ar.append(self.y0)
+            vy_ar.append(self.vy0)
+
+        return x_ar, vx_ar, y_ar, vy_ar
+
 
     def solve_func(self, i):
-       
-        sol = odeint(self.func, self.s0, (t[i], t[i+1]))
+        self.x = self.solver()[0][i]
+        self.vx = self.solver()[1][i]
+        self.y = self.solver()[2][i]
+        self.vy = self.solver()[3][i]
 
-        self.x = sol[1, 0]
-        self.y = sol[1, 2]
-        self.vx = sol[1, 1]
-        self.vy = sol[1, 3]
-
-        self.s0 = self.x, self.vx, self.y, self.vy
-
-        return self.x / ae, self.y / ae
+        return self.x / var.ae, self.y / var.ae
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import interaction as i
+import variables as var
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
@@ -6,19 +7,23 @@ from kivy.graphics import Ellipse, Color, Line
 from kivy.uix.label import Label
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from random import randint
+from kivy.uix.image import Image
+from kivy.properties import ObjectProperty
 
 # from kivy.config import Config
 # Config.set('graphics', 'resizable', 0)
-# Config.set('graphics', 'width', i.w)
-# Config.set('graphics', 'height', i.h)
+# Config.set('graphics', 'width', var.w)
+# Config.set('graphics', 'height', var.h)
 
-from kivy.lang import Builder
 Builder.load_file('center.kv')
 
 DRAG_START = ()
 DRAG_END = ()
 DRAGGING = False
+
 
 class Object(Widget):
     def __init__(self, **kw):
@@ -36,24 +41,24 @@ class Object(Widget):
 
         for j in range(self.step):
             self.COORDS.append(
-                (float(self.interect.solve_func(j)[0])*100+i.w,
-                 float(self.interect.solve_func(j)[1])*100+i.h))
+                (float(self.interect.solve_func(j)[0])*100+var.w,
+                 float(self.interect.solve_func(j)[1])*100+var.h))
 
     def draw(self):
         self.canvas.add(self.color)
-        self.ellipse = Ellipse(pos=self.pos, size=(i.r, i.r))
+        self.ellipse = Ellipse(pos=self.pos, size=(var.r, var.r))
         self.canvas.add(self.ellipse)
 
     def move(self, c):
         self.pos = Vector(self.COORDS[c])
         self.ellipse.pos = self.pos
 
-    
+
 class Move(Widget):
     def __init__(self):
         super(Move, self).__init__()
         self.counter = 0
-        self.n = i.T
+        self.n = var.t_end
 
     def update(self, dt):
         if self.counter == self.n - 1:
@@ -75,19 +80,24 @@ class Painter(Widget):
     def __init__(self, **kw):
         super(Painter, self).__init__(**kw)
         Window.bind(mouse_pos=self.mouse_pos)
-        
+
     def mouse_pos(self, window, pos):
         if DRAGGING == True:
             self.drawLine(pos)
 
     # отрисовка траектории
     def drawLine(self, mPos):
-        # x и y векторы 
+        # x и y векторы
         x_vector = self.x1 - mPos[0]
         y_vector = self.y1 - mPos[1]
 
         # модуль векторов
         vector_modul = (x_vector**2+y_vector**2)**(1/2)
+
+        # расстояние от центра
+        x_r = var.w - self.x1
+        y_r = var.h - self.y1
+        r_orbit = round(((x_r**2 + y_r**2)**0.5)/100, 1)
 
         try:
             cos_phi = x_vector/vector_modul
@@ -98,7 +108,7 @@ class Painter(Widget):
         except ZeroDivisionError:
             sin_phi = 0
 
-        v_modul = i.k*vector_modul
+        v_modul = var.k*vector_modul
 
         # скорости
         self.vx = v_modul*cos_phi
@@ -108,16 +118,20 @@ class Painter(Widget):
         with self.canvas.after:
             self.label = Label(text=f'Скорость: {int(v_modul)} м/с',
                                pos=(self.x1, self.y1))
+            self.label_center = Label(text=f'Расстояние до центра: {r_orbit} ae',
+                               pos=(self.x1, self.y1-30))
 
         self.canvas.clear()
         with self.canvas:
-            self.line = Line(points=[DRAG_START[0]+i.r/2, DRAG_START[1]+i.r/2, mPos[0], mPos[1]],
+            self.line = Line(points=[DRAG_START[0]+var.r/2, DRAG_START[1]+var.r/2, mPos[0], mPos[1]],
                              width=1.4)
+            self.line_center = Line(points=[DRAG_START[0]+var.r/2, DRAG_START[1]+var.r/2, var.w, var.h],
+                             width=1)
 
     def on_touch_down(self, touch):
         with self.canvas.before:
             self.color = Color(1, randint(0,1), randint(0,1), 1)
-            self.ellipse = Ellipse(pos=(touch.x, touch.y), size=(i.r, i.r))
+            self.ellipse = Ellipse(pos=(touch.x, touch.y), size=(var.r, var.r))
 
         self.x1 = touch.x
         self.y1 = touch.y
@@ -129,13 +143,14 @@ class Painter(Widget):
     def on_touch_up(self, touch):
         # убирание начальных элементов
         self.canvas.children.remove(self.line)
+        self.canvas.children.remove(self.line_center)
         self.canvas.before.remove(self.ellipse)
         self.canvas.after.clear()
 
         # задание данных объекту
         self.object = Move()
         self.object.create(color=self.color,
-                           pos=(((self.x1-i.w)/100)*i.ae, ((self.y1-i.h)/100)*i.ae),
+                           pos=(((self.x1-var.w)/100)*var.ae, ((self.y1-var.h)/100)*var.ae),
                            vel=(self.vx, self.vy))
         Clock.schedule_interval(self.object.update, .04)
         self.parent.add_widget(self.object)
@@ -143,6 +158,11 @@ class Painter(Widget):
         global DRAGGING, DRAG_START
         DRAGGING = False
 
+        # self.parent.remove_widget(self.object)
+
+class CustomLayout(FloatLayout):
+    background_image = ObjectProperty(Image(source='space.jpg'))
+    # pass
 
 class PlanetApp(App):
     def build(self):
